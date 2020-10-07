0eaf563c9934b954f7af246790a9a90937014c86
iqbalirfan65 <71933870+iqbalirfan65@users.noreply.github.com>
2020-10-08 01:04:18 +0700

    Add files via upload

---
+++ Dollar.js
@@ -0,0 +1,58 @@
+var baris = 3;
+var polaBintang = "*";
+var dolar = "$"
+var polaSamaDengan = "=";
+var hasil = "";
+
+console.log(baris)
+for (let index = 2; index < 5; index++) {
+
+  if ((index % 2) == 0) {
+    hasil = hasil.concat(polaSamaDengan);
+  } else {
+    hasil = hasil.concat(polaBintang);
+  }
+
+}
+console.log("" + dolar + hasil + dolar);
+
+hasil = "";
+
+for (let index = 2; index < 3; index++) {
+  if ((index % 2) == 0) {
+    hasil = hasil.concat(polaSamaDengan);
+  } else {
+    hasil = hasil.concat(polaBintang);
+  }
+
+}
+console.log(" " + dolar+ hasil+dolar);
+console.log("  " + dolar);
+
+
+//cepat 
+var baris=16;
+var polaBintang= "*";
+var polaSamaDengan= "=";
+var spasi =  "";
+var dolar = "$";
+for(let index = baris; index > 0; index--){
+
+    var hasil="";
+
+    var jmplhcell = (index*2)-1;
+    //console.log(jmplhcell);
+
+    for (let index2 = jmplhcell; index2 > 1; index2--){
+        if ((index2 %2)==0){
+         hasil = hasil.concat(polaSamaDengan);
+        }else if((index2 /1)==jmplhcell){
+          hasil = hasil.concat(dolar);
+        }else{
+         hasil = hasil.concat(polaBintang);
+        } 
+   
+    }
+    console.log(spasi+hasil+dolar)
+    spasi += " ";
+}
