6917eefb2e7b77cc203e96d9899f1dd128568e81
iqbalirfan65 <71933870+iqbalirfan65@users.noreply.github.com>
2020-10-14 20:58:02 +0700

    Add files via upload

---
+++ Kalkulatoer.js
@@ -0,0 +1,15 @@
+var input1 = 9;
+var input2 = 3;
+var operation = "/";
+
+        if (operation == "+") {
+            total = input1 + input2;
+        } else if (operation == "-") {
+            total = input1 - input2;
+        } else if (operation == "x") {
+            total = input1 * input2;
+        } else if (operation == "/") {
+            total = input1 / input2;
+        }
+
+console.log(total);
